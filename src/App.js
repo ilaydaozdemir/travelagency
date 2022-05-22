77b28485c0076b0d66c5f5e3320415e7c3513500
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-22 17:13:08 +0300

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Services />
     <Recommend />
     <Testimonials />
+    <Footer />
 
 
   </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+import { BsLinkedin, BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <span>Copyright &copy;2022 TravelAgency. All rights reserved</span>
+      <ul className="links">
+        <li>
+          <a href="#hero">Home</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#recommend">Places</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+      </ul>
+      <ul className="social_links">
+        <li>
+          <BsFacebook />
+        </li>
+        <li>
+          <BsLinkedin />
+        </li>
+        <li>
+          <AiFillInstagram />
+        </li>
+      </ul>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #d0d8ff;
+  padding: 2.5rem;
+  border-radius: 0.5rem;
+  ul {
+    display: flex;
+    list-style-type: none;
+    gap: 2rem;
+    li {
+      a {
+        text-decoration: none;
+        color: black;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #302ce9;
+          cursor: pointer;
+        }
+      }
+      svg {
+        font-size: 1.3rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #302ce9;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
