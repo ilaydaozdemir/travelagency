42f38c0f5efa24dce8fea8d98f9d9fc44b914b9b
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-22 21:28:14 +0300

    Add ScrollToTop component

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 const ScrollToTop = () => {
-  return <div>ScrollToTop</div>;
+  const [scrollState, setScrollState] = useState(false);
+
+  const toTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 200 ? setScrollState(true) : setScrollState(false);
+  });
+  return (
+    <ToTop onClick={toTop} scrollState={scrollState}>
+      <img src={logo} />
+    </ToTop>
+  );
 };
 
 export default ScrollToTop;
+const ToTop = styled.div`
+  display: ${({ scrollState }) => (scrollState ? "block" : "none")};
+  position: fixed;
+  bottom: 1rem;
+  right: 2rem;
+  z-index: 10;
+  cursor: pointer;
+  img {
+    height: 1.5rem;
+  }
+  border-radius: 2rem;
+  background-color: #1900ff39;
+  padding: 1rem;
+`;
