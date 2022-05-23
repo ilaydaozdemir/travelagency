cfa276e69ab6a9106ad8eab0516809a2dfd22bd6
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-23 13:42:59 +0300

    Add css for responsive Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -96,4 +96,18 @@ const Section = styled.section`
       }
     }
   }
+  @media screen and (min-width:280px)and (max-width:768px){
+    .testimonials{
+      flex-direction: column;
+      margin: 0;
+      .testimonial{
+        justify-content: center;
+        .info{
+          flex-direction: column;
+          justify-content: center;
+        }
+      }
+    }
+
+  }
 `;
